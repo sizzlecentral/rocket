a96ecb13ebb406fd84c9d92366a0df27b8f5c8dd
Don Cooper <don_cooper@hotmail.com>
2017-06-22 11:42:28 -0400

    Test 1 clear

--- rocket_test.rb
+++ rocket_test.rb
@@ -2,5 +2,24 @@ require "minitest/autorun"
 require_relative "rocket"
 
 class RocketTest < Minitest::Test
-  # Write your tests here!
+
+  def test_name_returns_random_name
+
+    #arrange
+    rocket = Rocket.new
+
+    #act
+    name = rocket.name
+
+    #assert
+    assert name != nil
+
+  end
+
+
+
+
+
+
+
 end
